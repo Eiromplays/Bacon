88584aaa4b114ba2dc39f4b4918a2e5857438022
Eirik Sjøløkken <eiromplays@protonmail.com>
2026-05-03 20:06:24 +0200

    Make BaconJsonSerializer handle UTF-8 properly with UnsafeRelaxedJsonEscaping

--- src/Bacon.Web/BaconJsonSerializer.cs
+++ src/Bacon.Web/BaconJsonSerializer.cs
@@ -1,3 +1,4 @@
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using Bacon.Compiler.Evaluation;
 
@@ -5,10 +6,19 @@ namespace Bacon.Web;
 
 public static class BaconJsonSerializer
 {
+    private static readonly JsonSerializerOptions Options = new()
+    {
+        // UnsafeRelaxedJsonEscaping is safe for API responses with application/json content-type.
+        // It allows non-ASCII characters (like Norwegian å, ø, æ) to be sent as UTF-8 directly
+        // instead of being escaped as \u00e5 etc. The "unsafe" prefix refers to potential issues
+        // when JSON is embedded in HTML, which doesn't apply to API responses.
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+
     public static string Serialize(BaconValue value)
     {
         var obj = ToJsonCompatible(value);
-        return JsonSerializer.Serialize(obj);
+        return JsonSerializer.Serialize(obj, Options);
     }
 
     private static object? ToJsonCompatible(BaconValue value) => value switch
