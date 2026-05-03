916a9b75d70251f875d38f9519ad2f267fa04e0a
Eirik Sjøløkken <eiromplays@protonmail.com>
2026-05-03 09:38:37 +0200

    Add stdlib, CLI, and examples

--- Bacon.Simple/Program.cs
+++ Bacon.Simple/Program.cs
@@ -1,19 +1,42 @@
-﻿using Bacon.Compiler.Ast;
+﻿using Bacon.Compiler.Evaluation;
 using Bacon.Compiler.Lexing;
 using Bacon.Compiler.Parsing;
 
-const string source = """
-                      prosess sjekkAlder(person) {
-                          hvis person.alder større eller lik 18 {
-                              leverer "voksen"
-                          } ellers hvis person.alder større enn 12 {
-                              leverer "tenåring"
-                          } ellers {
-                              leverer "barn"
-                          }
-                      }
-                      """;
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Bruk: bacon <fil.bacon>");
+    return 1;
+}
 
-var tokens = Lexer.Tokenize(source);
-var program = Parser.Parse(tokens);
-Console.WriteLine(AstPrinter.Print(program));
+var filePath = args[0];
+
+if (!File.Exists(filePath))
+{
+    Console.Error.WriteLine($"Fil ikke funnet: {filePath}");
+    return 1;
+}
+
+var source = await File.ReadAllTextAsync(filePath);
+
+try
+{
+    var tokens = Lexer.Tokenize(source);
+    var program = Parser.Parse(tokens);
+    Evaluator.Evaluate(program);
+    return 0;
+}
+catch (LexerException ex)
+{
+    Console.Error.WriteLine($"Lexer-feil: {ex.Message}");
+    return 1;
+}
+catch (ParseException ex)
+{
+    Console.Error.WriteLine($"Parser-feil: {ex.Message}");
+    return 1;
+}
+catch (RuntimeException ex)
+{
+    Console.Error.WriteLine($"Runtime-feil: {ex.Message}");
+    return 1;
+}
